a4e6c6e8993e428c375a468f0a28fe78a5b6cbd9
Jung Kyunghun <jung.kyunghun@quallitybits.net>
2019-05-23 14:09:24 +0900

    2-b

--- FlappyBirdLike/GameScene.swift
+++ FlappyBirdLike/GameScene.swift
@@ -12,7 +12,37 @@ import GameplayKit
 class GameScene: SKScene {
 
     override func didMove(to view: SKView) {
+        let land = SKSpriteNode(imageNamed: "land")
+        land.position = CGPoint(x: self.size.width / 2, y: 50)
+        land.zPosition = 3
+        self.addChild(land)
         
+        let sky = SKSpriteNode(imageNamed: "sky")
+        sky.position = CGPoint(x: self.size.width / 2, y: 100)
+        sky.zPosition = 1
+        self.addChild(sky)
+        
+        let bird = SKSpriteNode(imageNamed: "bird")
+        bird.position = CGPoint(x: self.size.width / 2, y: 200)
+        bird.zPosition = 2
+        self.addChild(bird)
+        
+        let ceiling = SKSpriteNode(imageNamed: "ceiling")
+        ceiling.position = CGPoint(x: self.size.width / 2, y: 300)
+        ceiling.zPosition = 3
+        self.addChild(ceiling)
+        
+        let pipeUp = SKSpriteNode(imageNamed: "pipe")
+        pipeUp.position = CGPoint(x: self.size.width / 2, y: 100)
+        pipeUp.zPosition = 2
+        self.addChild(pipeUp)
+        
+        let pipeDown = SKSpriteNode(imageNamed: "pipe")
+        pipeDown.position = CGPoint(x: self.size.width / 2, y: self.size.height)
+        pipeDown.zPosition = 2
+        pipeDown.xScale = -1
+        pipeDown.zRotation = .pi
+        self.addChild(pipeDown)
     }
     
 }
