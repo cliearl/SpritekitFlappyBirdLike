54a1a05b1caed16ffb6f952176611ecb0dc7e915
Jung Kyunghun <jung.kyunghun@quallitybits.net>
2019-05-24 12:46:05 +0900

    03

--- FlappyBirdLike/GameScene.swift
+++ FlappyBirdLike/GameScene.swift
@@ -11,20 +11,42 @@ import SpriteKit
 class GameScene: SKScene {
 
     override func didMove(to view: SKView) {
+        createBird()
+        createEnvironment()
+        
+    }
+    
+    func createBird() {
+//        let birdArray = SKTextureAtlas(named: "Bird")
+        
+        let bird = SKSpriteNode(imageNamed: "bird1")
+        bird.position = CGPoint(x: self.size.width / 2, y: 350)
+        bird.zPosition = 4
+        self.addChild(bird)
+        
+        // 애니메이션 파트
+//        var aniArray = [SKTexture]()
+//        for i in 1...birdArray.textureNames.count {
+//            aniArray.append(SKTexture(imageNamed: "bird\(i)"))
+//        }
+//        let flyingAnimation = SKAction.animate(with: aniArray, timePerFrame: 0.1)
+//        bird.run(SKAction.repeatForever(flyingAnimation))
+        
+        guard let flyingBySKS = SKAction(named: "flying") else { return }
+        bird.run(flyingBySKS)
+    }
+    
+    func createEnvironment() {
         let land = SKSpriteNode(imageNamed: "land")
         land.position = CGPoint(x: self.size.width / 2, y: 50)
         land.zPosition = 3
         self.addChild(land)
+        print(self.size.width / land.size.width)
         
         let sky = SKSpriteNode(imageNamed: "sky")
         sky.position = CGPoint(x: self.size.width / 2, y: 100)
         sky.zPosition = 1
         self.addChild(sky)
-        
-        let bird = SKSpriteNode(imageNamed: "bird")
-        bird.position = CGPoint(x: self.size.width / 2, y: 350)
-        bird.zPosition = 4
-        self.addChild(bird)
         
         let ceiling = SKSpriteNode(imageNamed: "ceiling")
         ceiling.position = CGPoint(x: self.size.width / 2, y: self.size.height)
