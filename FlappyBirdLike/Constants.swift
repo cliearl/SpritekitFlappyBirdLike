ea1ae828e816bc9f137f630c7ce42886d39dbf2e
Jung Kyunghun <jung.kyunghun@quallitybits.net>
2019-05-27 18:51:11 +0900

    Lecture4

---
+++ FlappyBirdLike/Constants.swift
@@ -0,0 +1,17 @@
+//
+//  Constants.swift
+//  FlappyBirdLike
+//
+//  Created by Kyunghun Jung on 24/05/2019.
+//  Copyright © 2019 qualitybits.net. All rights reserved.
+//
+
+import SpriteKit
+
+struct Layer {
+    static let sky: CGFloat = 1
+    static let pipe: CGFloat = 2
+    static let land: CGFloat = 3
+    static let ceiling: CGFloat = 4
+    static let bird: CGFloat = 5
+}
--- FlappyBirdLike/GameScene.swift
+++ FlappyBirdLike/GameScene.swift
@@ -11,6 +11,9 @@ import SpriteKit
 class GameScene: SKScene {
 
     override func didMove(to view: SKView) {
+        let bgColor = SKColor(red: 81.0 / 255.0, green: 192.0 / 255.0, blue: 201.0 / 255.0, alpha: 1.0)
+        self.backgroundColor = bgColor
+        
         createBird()
         createEnvironment()
         setupPipe()
@@ -19,39 +22,72 @@ class GameScene: SKScene {
     func createBird() {        
         let bird = SKSpriteNode(imageNamed: "bird1")
         bird.position = CGPoint(x: self.size.width / 2, y: 350)
-        bird.zPosition = 4
-        self.addChild(bird)
+        bird.zPosition = Layer.bird
+        addChild(bird)
         
         guard let flyingBySKS = SKAction(named: "flying") else { return }
         bird.run(flyingBySKS)
     }
     
     func createEnvironment() {
-        let land = SKSpriteNode(imageNamed: "land")
-        land.position = CGPoint(x: self.size.width / 2, y: 50)
-        land.zPosition = 3
-        self.addChild(land)
+        let envAtlas = SKTextureAtlas(named: "Environment")
+        let landTexture = envAtlas.textureNamed("land")
+        let landRepeatNum = Int(ceil(self.size.width / landTexture.size().width))
+        let skyTexture = envAtlas.textureNamed("sky")
+        let skyRepeatNum = Int(ceil(self.size.width / skyTexture.size().width))
+        let ceilTexture = envAtlas.textureNamed("ceiling")
+        let ceilRepearNum = Int(ceil(self.size.width / ceilTexture.size().width))
         
-        let sky = SKSpriteNode(imageNamed: "sky")
-        sky.position = CGPoint(x: self.size.width / 2, y: 100)
-        sky.zPosition = 1
-        self.addChild(sky)
+        for i in 0...landRepeatNum {
+            let land = SKSpriteNode(texture: landTexture)
+            land.anchorPoint = CGPoint.zero
+            land.position = CGPoint(x: CGFloat(i) * land.size.width, y: 0)
+            land.zPosition = Layer.land
+            
+            addChild(land)
+            
+            let moveLeft = SKAction.moveBy(x: -landTexture.size().width, y: 0, duration: 20)
+            let moveReset = SKAction.moveBy(x: landTexture.size().width, y: 0, duration: 0)
+            let moveSequence = SKAction.sequence([moveLeft, moveReset])
+            land.run(SKAction.repeatForever(moveSequence))
+        }
         
-        let ceiling = SKSpriteNode(imageNamed: "ceiling")
-        ceiling.position = CGPoint(x: self.size.width / 2, y: self.size.height)
-        ceiling.zPosition = 3
-        self.addChild(ceiling)
+        for i in 0...skyRepeatNum {
+            let sky = SKSpriteNode(texture: skyTexture)
+            sky.anchorPoint = CGPoint.zero
+            sky.position = CGPoint(x: CGFloat(i) * sky.size.width, y: envAtlas.textureNamed("land").size().height)
+            sky.zPosition = Layer.sky
+            addChild(sky)
+            
+            let moveLeft = SKAction.moveBy(x: -skyTexture.size().width, y: 0, duration: 40)
+            let moveReset = SKAction.moveBy(x: skyTexture.size().width, y: 0, duration: 0)
+            let moveSequence = SKAction.sequence([moveLeft, moveReset])
+            sky.run(SKAction.repeatForever(moveSequence))
+        }
+        
+        for i in 0...ceilRepearNum {
+            let ceiling = SKSpriteNode(texture: ceilTexture)
+            ceiling.anchorPoint = CGPoint.zero
+            ceiling.position = CGPoint(x: CGFloat(i) * ceiling.size.width, y: self.size.height - ceiling.size.height / 2)
+            ceiling.zPosition = Layer.ceiling
+            addChild(ceiling)
+            
+            let moveLeft = SKAction.moveBy(x: -ceilTexture.size().width, y: 0, duration: 3)
+            let moveReset = SKAction.moveBy(x: ceilTexture.size().width, y: 0, duration: 0)
+            let moveSequence = SKAction.sequence([moveLeft, moveReset])
+            ceiling.run(SKAction.repeatForever(moveSequence))
+        }
     }
     
     func setupPipe() {
         let pipeDown = SKSpriteNode(imageNamed: "pipe")
         pipeDown.position = CGPoint(x: self.size.width / 2, y: 0)
-        pipeDown.zPosition = 2
+        pipeDown.zPosition = Layer.pipe
         self.addChild(pipeDown)
         
         let pipeUp = SKSpriteNode(imageNamed: "pipe")
         pipeUp.position = CGPoint(x: self.size.width / 2, y: self.size.height + 100)
-        pipeUp.zPosition = 2
+        pipeUp.zPosition = Layer.pipe
         pipeUp.xScale = -1
         pipeUp.zRotation = .pi
         self.addChild(pipeUp)
