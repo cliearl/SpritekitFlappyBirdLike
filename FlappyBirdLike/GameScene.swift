f7c56912e6cae69fdc86d4ed20d9be99f5d198c4
Jung Kyunghun <jung.kyunghun@quallitybits.net>
2019-06-04 15:24:44 +0900

    Ch 7

--- FlappyBirdLike/GameScene.swift
+++ FlappyBirdLike/GameScene.swift
@@ -8,9 +8,20 @@
 
 import SpriteKit
 
+enum GameState {
+    case ready
+    case playing
+    case dead
+}
+
 class GameScene: SKScene, SKPhysicsContactDelegate {
     
     var bird = SKSpriteNode()
+    var gameState = GameState.ready {
+        didSet {
+            print(gameState)
+        }
+    }
     
     var score:Int = 0 {
         didSet {
@@ -29,7 +40,6 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         
         createBird()
         createEnvironment()
-        createInfinitePipe(duration: 4)
         createScore()
     }
     
@@ -54,7 +64,7 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         bird.physicsBody?.contactTestBitMask = PhysicsCategory.land | PhysicsCategory.pipe | PhysicsCategory.ceiling | PhysicsCategory.score
         bird.physicsBody?.collisionBitMask = PhysicsCategory.land | PhysicsCategory.pipe | PhysicsCategory.ceiling
         bird.physicsBody?.affectedByGravity = true
-        bird.physicsBody?.isDynamic = true
+        bird.physicsBody?.isDynamic = false
         addChild(bird)
         
         guard let flyingBySKS = SKAction(named: "flying") else { return }
@@ -181,8 +191,22 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
     
     // MARK: - Game Algorithm
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        self.bird.physicsBody?.velocity = CGVector(dx: 0, dy: 0)
-        self.bird.physicsBody?.applyImpulse(CGVector(dx: 0, dy: 7))
+        switch gameState {
+        case .ready:
+            gameState = .playing
+            
+            self.bird.physicsBody?.isDynamic = true
+            self.bird.physicsBody?.applyImpulse(CGVector(dx: 0, dy: 10))
+            self.createInfinitePipe(duration: 4)
+            
+        case .playing:
+            self.bird.physicsBody?.velocity = CGVector(dx: 0, dy: 0)
+            self.bird.physicsBody?.applyImpulse(CGVector(dx: 0, dy: 7))
+        case .dead:
+            let scene = GameScene(size: self.size)
+            let transition = SKTransition.doorsOpenHorizontal(withDuration: 1)
+            self.view?.presentScene(scene, transition: transition)
+        }
     }
     
     func didBegin(_ contact: SKPhysicsContact) {
@@ -198,15 +222,22 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         switch collideType {
         case PhysicsCategory.land:
             print("land!")
+            gameOver()
         case PhysicsCategory.ceiling:
             print("ceiling!")
         case PhysicsCategory.pipe:
             print("pipe!")
+            gameOver()
         case PhysicsCategory.score:
             score += 1
             print(score)
         default:
             break
         }
+    }
+    
+    func gameOver() {
+        self.gameState = .dead
+        self.isPaused = true
     }
 }
